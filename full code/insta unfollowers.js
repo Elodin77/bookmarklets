65bfa6479bae60d6d2e63ad5fd87fcece2c1ce1e
Elodin77 <developerdjay@gmail.com>
2023-02-08 18:17:07 -0800

    test

--- full code/insta unfollowers.js
+++ full code/insta unfollowers.js
@@ -2,15 +2,20 @@ var d = document.querySelectorAll('*[style="height: auto; overflow: hidden auto;
 var c = 0;
 var timer = setInterval(function () { 
   d.scrollIntoView(false);
-  if (c != d.querySelectorAll("a").length) {
-    c = d.querySelectorAll("a").length;
+  var sel = d.querySelectorAll("a");
+  if (c != sel.length) {
+    for (var i=c; i<sel.length; i++) {
+      setTimeout(function(x) {
+        x.innerHTML = "☠";
+      },Math.random()*2000,sel[i])
+    }
+    c = sel.length;
   } else {
     clearInterval(timer);
     var a = d.querySelectorAll("a");
     var l = [];
     for (var i = 0; i < a.length; i++) {
       let s = a[i].href.split("/");
-      console.log(s);
       s = s[s.length - 2];
       if (!l.includes(s)) {
         l.push(s);
@@ -20,5 +25,7 @@ var timer = setInterval(function () {
     for (var i=0; i<l.length; i++) {
       document.write(l[i] + "<br>");
     }
+    document.body.style.color = "white";
+    document.body.style.backgroundColor = "black";
   }
 }, 2000);
